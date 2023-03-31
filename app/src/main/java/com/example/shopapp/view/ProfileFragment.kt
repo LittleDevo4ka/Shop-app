beda5716d10f09b2aa27369278329e2e96eb1c86
LittleDovo4ka <Dorofey0.8.0@gmail.com>
2023-03-31 15:20:56 +0300

    ADD: three profile screens

--- app/src/main/java/com/example/shopapp/view/ProfileFragment.kt
+++ app/src/main/java/com/example/shopapp/view/ProfileFragment.kt
@@ -1,60 +1,111 @@
 package com.example.shopapp.view
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.shopapp.R
+import com.example.shopapp.databinding.FragmentProfileBinding
+import com.example.shopapp.databinding.LogInProfileBinding
+import com.example.shopapp.databinding.RegisterProfileBinding
+import com.example.shopapp.databinding.StartProfileBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        addStartProfile()
+    }
+
+    private fun addStartProfile() {
+
+        val addStartProfileBinding = StartProfileBinding.inflate(layoutInflater,
+            binding.placeForProfile, false)
+
+        addStartProfileBinding.registerButtonStartProfile.setOnClickListener{
+            binding.placeForProfile.removeAllViews()
+
+            addRegisterProfile()
+        }
+
+        addStartProfileBinding.logInButtonStartProfile.setOnClickListener{
+            binding.placeForProfile.removeAllViews()
+
+            addLogInProfile()
+        }
+
+        binding.placeForProfile.addView(addStartProfileBinding.root)
+    }
+
+    private fun addLogInProfile() {
+        val addLogInProfileBinding = LogInProfileBinding.inflate(layoutInflater,
+            binding.placeForProfile, false)
+
+        addLogInProfileBinding.registerButtonLogInProfile.setOnClickListener{
+            binding.placeForProfile.removeAllViews()
+
+            addRegisterProfile()
+        }
+
+        binding.placeForProfile.addView(addLogInProfileBinding.root)
+    }
+
+    private fun addRegisterProfile() {
+        val addRegisterProfileBinding = RegisterProfileBinding.inflate(layoutInflater,
+            binding.placeForProfile, false)
+
+        addRegisterProfileBinding.passwordEdittextRegisterProfile
+            .addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if(p0 != null) {
+                    if(p0.isNotEmpty()) {
+                        addRegisterProfileBinding.repeatPasswordLayoutRegisterProfile.isEnabled = true
+                    } else {
+                        addRegisterProfileBinding.repeatPasswordEdittextRegisterProfile
+                            .setText("")
+
+                        addRegisterProfileBinding.repeatPasswordLayoutRegisterProfile
+                            .isEnabled = false
+                    }
                 }
             }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
+
+        addRegisterProfileBinding.repeatPasswordLayoutRegisterProfile.isEnabled = false
+
+        addRegisterProfileBinding.logInButtonRegisterProfile.setOnClickListener{
+            binding.placeForProfile.removeAllViews()
+
+            addLogInProfile()
+        }
+
+        binding.placeForProfile.addView(addRegisterProfileBinding.root)
+    }
+
+    private fun addProfile() {
+
     }
 }
